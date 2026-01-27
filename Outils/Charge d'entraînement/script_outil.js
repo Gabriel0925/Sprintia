c6e966a20a71ccef8214dcdc91da31ef4f29d462
Gabriel0925 <gabchap486@gmail.com>
2026-01-27 21:35:51 +0100

    Correction important bug in setting page and optimizing code for more durability.  Optimizing new interface in tools and deleted old code and adapting code for new design of tools page. Added new fonctionnality : training load feature

--- Outils/Charge d'entraînement/script_outil.js
+++ Outils/Charge d'entraînement/script_outil.js
@@ -33,18 +33,118 @@ async function RecupValueGraphique() {
     return {ChargeDatas, DateDatas}
 }
 
-async function Initialisation() {
-    text = "Sprintia n'a pas encore assez de données pour analyser votre charge d'entraînement. Pas de panique vous avez juste besoin d'ajouter un entraînement pour que Sprintia analyse vos entraînements."
+function InterpretationJRM(ChargeAigue, ChargeChronique, AnalysePossible) {
+    // Initialisation 
+    let Interpretation = "Sprintia n'a pas encore assez de données pour analyser votre charge d'entraînement. Pas de panique vous avez juste besoin d'ajouter 5 entraînements pour que Sprintia analyse vos entraînements."
+    let Ratio = 0
 
-    let initialisationJRM  = document.getElementById("reponse-coach-indulgence")
-    if (initialisationJRM) {
-        initialisationJRM.textContent = text
+    // Vérif si possible d'analyser
+    if (AnalysePossible == false) {
+        return Interpretation
     }
 
+    // Dico des phrases
+    const PhraseJRM = [
+        "Vous êtes en train de perdre du niveau, attention !<br>Vous pourriez augmenter l'intensité de vos entraînements si vous voulez basculer en mode progression optimale et améliorer vos performances.",
+        "Charge idéale pour améliorer vos performances !<br>Continuez comme ça pour progresser ! Gardez cette même régularité dans vos entraînements pour rester en mode progression optimale.",
+        "Risque élevé de blessure !<br>Prenez quelques jours de pause pour laisser votre corps récupérer et réduire les risques de blessure."
+    ]
 
-    // attendre la recup des datas
+    // Calcul du ratio
+    ChargeChronique = ChargeChronique/4 // on met charge chronique par semaine pr le ratio
+    Ratio = ChargeAigue/ChargeChronique
+
+    if (Ratio <= 0.8) {
+        Interpretation = PhraseJRM[0]
+    } else if (Ratio <= 1.35) {
+        Interpretation = PhraseJRM[1]
+    } else if (Ratio >= 1.35) {
+        Interpretation = PhraseJRM[2]
+    }
+
+    return Interpretation
+}
+
+async function CalculCharge() {
+    // Initialisation 
+    let ChargeAigue = 0
+    let ChargeChronique = 0
+    let compteur = 0
+    let compteur2 = 0
+    let Charge = []
+    let DateCharge = []
+
+    // Date recup
+    const DateActuelle = new Date()
+    let DateMoins7J = new Date()
+    DateMoins7J = DateMoins7J.setDate(DateActuelle.getDate() - 7)
+    let DateMoins28J = new Date()
+    DateMoins28J = DateMoins28J.setDate(DateActuelle.getDate() - 7)
+
+    let DateBoucle = ""
+
+    // Recup data BDD
+    let HistoriqueDB = await db.entrainement.toArray() // recup de toutes les datas
+
+    let ChargeAigueLi = HistoriqueDB.map(data => data.charge_entrainement)
+    let DateLi = HistoriqueDB.map(data => data.date)
+
+    ChargeAigueLi.forEach(DataCharge => {
+        DateBoucle = DateLi[compteur]
+
+        if (DateBoucle >= DateMoins28J) {
+            Charge.push(DataCharge)
+            DateCharge.push(DateBoucle)
+        }
+
+        compteur += 1
+    });
+
+    // A completer
+    let AnalysePossible = true
+    if (Charge.length < 4) {
+        AnalysePossible = false
+    }
+
+    DateCharge.forEach(DateEntrainement => {
+        if (DateEntrainement >= DateMoins7J) {
+            ChargeAigue = ChargeAigue + parseFloat(Charge[compteur2])
+            ChargeChronique = ChargeChronique + parseFloat(Charge[compteur2])
+        } else if (DateEntrainement >= DateMoins28J) {
+            ChargeChronique = ChargeChronique + parseFloat(Charge[compteur2])
+        }
+
+        compteur2 += 1
+    });
+
+    // Arrondi
+    ChargeAigue = ChargeAigue.toFixed(1).replace(".", ",")
+    ChargeChronique = ChargeChronique.toFixed(1).replace(".", ",")
+
+    return {ChargeAigue, ChargeChronique, AnalysePossible}
+}
+
+async function Initialisation() {
+    // recup zone html ou il y a linterpretation JR%
+    let HTMLInterpretationJRM  = document.getElementById("reponse-coach-indulgence")
+
+    // recup des charges plus interpretation et affichage
     let {ChargeDatas, DateDatas} = await RecupValueGraphique()
+    
+    let {ChargeAigue, ChargeChronique, AnalysePossible} = await CalculCharge()
 
+    if (ChargeAigue) {
+        document.getElementById("charge-7j").textContent = ChargeAigue
+        document.getElementById("charge-28j").textContent = ChargeChronique
+    }
+
+    let Interpretation = InterpretationJRM(parseInt(ChargeAigue), parseInt(ChargeChronique), AnalysePossible)
+
+    if (Interpretation && HTMLInterpretationJRM) {
+        HTMLInterpretationJRM.innerHTML = Interpretation
+    }
+
+    // Generation Graphique
     if (ChargeDatas.length > 0) { // Si il y a chargedata data il y a forcement date data
         // Ajout de la class pr le faire apparaitre
         document.getElementById("conteneur-graphique").classList.add("visible")
