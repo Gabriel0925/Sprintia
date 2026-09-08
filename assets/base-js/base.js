8e67bad72423371426dae09bc39bf7b3edbb0db1
Gabriel0925 <gabchap486@gmail.com>
2026-09-08 20:30:43 +0200

    Correction de bug

--- assets/base-js/base.js
+++ assets/base-js/base.js
@@ -1,5 +1,5 @@
 // VARIABLE GLOBALE A TOUTES LES PAGES SPRINTIA
-const VERSION_SPRINTIA = "v26.10 beta"
+const VERSION_SPRINTIA = "v26.10 b3"
 
 function newMajPointBleu() {
     let lastViewVersion = localStorage.getItem("lastViewVersion") || "SPRINTIA 5.2"
