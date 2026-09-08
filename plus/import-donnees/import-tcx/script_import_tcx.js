725d1da7f71a04754d94d21eef02e0a3abbc491c
Gabriel0925 <gabchap486@gmail.com>
2026-09-08 19:50:32 +0200

    Correction de bug

--- plus/import-donnees/import-tcx/script_import_tcx.js
+++ plus/import-donnees/import-tcx/script_import_tcx.js
@@ -250,9 +250,9 @@ async function uploadFileTCX(event) {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-    const inputFileInvisible = document.getElementById("file-input-tcx")
+    const inputFileInvisible = document.getElementById("file-input-tcx") 
     if (inputFileInvisible) {inputFileInvisible.addEventListener("change", (event) => {uploadFileTCX(event)})}
     
     const inputFile = document.getElementById("button-import-tcx")
-    if (inputFile) {inputFile.addEventListener("change", () => {document.getElementById('file-input-tcx').click()})}
+    if (inputFile) {inputFile.addEventListener("click", () => {document.getElementById('file-input-tcx').click()})}
 })
